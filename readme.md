45ea0409698e22bf9ddc0877a588365627e0a584
Inaki Villar <inaki.seri@gmail.com>
2023-02-27 15:01:54 -0800

    initial import

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Example Gradle Enterprise Maven Extension for Spotless and SetVer
---
+++ src/main/java/codes/hubert/maven/spotless/App.java
@@ -0,0 +1,15 @@
+/* (C)2013 */
+package codes.hubert.maven.spotless;
+
+/** Messy code makes programmers life harder. */
+public class App {
+
+  public static final String myConst =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"
+          + " labore ";
+
+  public static void main(String[] args) {
+    System.out.println(myConst);
+    System.out.println("Please" + "fix" + " me :)");
+  }
+}
